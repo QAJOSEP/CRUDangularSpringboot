48321bc747414322a14eb65d32be68984f8e3a10
Jose Developer <papat7442@gmail.com>
2023-01-05 13:02:12 -0600

    first commit

---
+++ servidor/tareas/src/main/java/app/tareas/TareaRepo.java
@@ -0,0 +1,12 @@
+package app.tareas;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin
+@Repository@RepositoryRestResource(path= "tareas", collectionResourceRel = "tareas")
+public interface TareaRepo extends JpaRepository<Tarea, Integer> {
+    
+}
